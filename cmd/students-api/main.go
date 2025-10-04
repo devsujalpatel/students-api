03167ad14d83832d42ef3a8502c80898b004557b
devsujalpatel <sujal7455@gmail.com>
2025-10-04 17:49:35 +0530

    Gracefull shutdown

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/devsujalpatel/students-api/internal/config"
 )
@@ -29,9 +35,33 @@ func main() {
 	}
 	fmt.Printf("Server started %s", cfg.HTTPServer.Addr)
 
-	err := server.ListenAndServe()
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+
+		err := server.ListenAndServe()
+
+		if err != nil {
+			log.Fatalf("Failed to start server %s", err.Error())
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	err := server.Shutdown(ctx)
 
 	if err != nil {
-		log.Fatalf("Failed to start server %s", err.Error())
+		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
 	}
+
+	slog.Info("server shutdown successfully")
+
 }
