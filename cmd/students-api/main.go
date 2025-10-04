f083e2c764626837bd77723cc11caac328df223a
devsujalpatel <sujal7455@gmail.com>
2025-10-04 17:32:31 +0530

    Project setup

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/devsujalpatel/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to students api")
+	// load config
+	cfg := config.MustLoad()
+
+	// database setup
+	// setup router
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students api"))
+	})
+
+	// setup server
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	fmt.Printf("Server started %s", cfg.HTTPServer.Addr)
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatalf("Failed to start server %s", err.Error())
+	}
 }
